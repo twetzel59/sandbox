2cab40bd6d76825e43fdf62afe405ce00a91baf9
twetzel59 <twetzel59@gmail.com>
2019-08-09 13:11:47 -0400

    Documentation fixes

--- src/timing.rs
+++ src/timing.rs
@@ -6,19 +6,19 @@
 
 use std::time::{Duration, Instant};
 
-// A timing primitive that keeps track of how much
-// time has passed since the last reset.
-//
-// It can be used to time the duration of a game
-// frame update.
-//
-// ``Clock`` is essentially a virtual stopwatch.
-// Upon construction, the clock begins "timing
-// automatically". Implmentation-wise, there is
-// no actual background work being executed.
-// Rather, the clock remembers a unique system
-// timestamp and, when queried, calcuates the
-// delta from that pinned time.
+/// A timing primitive that keeps track of how much
+/// time has passed since the last reset.
+///
+/// It can be used to time the duration of a game
+/// frame update.
+///
+/// ``Clock`` is essentially a virtual stopwatch.
+/// Upon construction, the clock begins "timing
+/// automatically". Implmentation-wise, there is
+/// no actual background work being executed.
+/// Rather, the clock remembers a unique system
+/// timestamp and, when queried, calcuates the
+/// delta from that pinned time.
 pub struct Clock {
     last_restart: Instant,
 }
